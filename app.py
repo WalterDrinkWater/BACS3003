6e2bfac5602465b9cb2bb34c567ef55a7e377fb7
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-09-19 01:53:55 +0800

    apply (test1)

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import boto3
 from config import *
 from io import BytesIO
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "sem-sk"
@@ -18,9 +19,51 @@ db_conn = connections.Connection(
 )
 output = {}
 
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('Index.html')
+
+
+@app.route("/application/apply", methods=['POST'])
+def apply():
+    studName = request.form["name"]
+    studIc = request.form["ic"]
+    studGender = request.form["gender"]
+    studAddress = request.form["address"]
+    studPostcode = request.form["postcode"]
+    studState = request.form["state"]
+    studCity = request.form["city"]
+    studPhone = request.form["phone"]
+    guardianName = request.form["guardName"]
+    guardianNo = request.form["guardNo"]
+    studEmail = request.form["email"]
+    studHealth = request.form["selectHealth"]
+    datetimeApplied = datetime.Now()
+
+    if(studHealth == 'Other'):
+        studHealth = request.form["others"]
+
+    fulladdress = studAddress + ", " + studPostcode + \
+        ", " + studState + ", " + studCity
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute("INSERT INTO Applications VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                       ("APP", studName, studIc,
+                        studGender, fulladdress, studEmail, datetimeApplied, studPhone, guardianName, guardianNo,
+                        studHealth, "PROGRAMME", "ACC"))
+        db_conn.commit()
+
+    except Exception as e:
+        return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template("AddEmpOutput.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
