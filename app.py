38957caae191616273512a49c3d45619969395fe
KinWeng <32710027+hoKingwin@users.noreply.github.com>
2023-09-19 11:27:30 +0800

    idk

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 import boto3
 from config import *
 from io import BytesIO
-import numpy as np
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "sem-sk"
@@ -21,19 +20,31 @@ output = {}
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('Index.html')
+    return render_template('Login.html')
 
 @app.route("/programme", methods=['GET', 'POST'])
 def Get_Programme():
     if request.method == 'GET':
-        cursor = db_conn.cursor()
-        cursor.execute("SELECT * FROM Programme ORDER BY ProgrammeID")
-        progDetails = cursor.fetchall()
-        cursor.execute("SELECT COUNT(Programme.programmeID) FROM Programme,ProgrammeCourse,Course WHERE Programme.programmeID = ProgrammeCourse.programmeID AND ProgrammeCourse.courseCode = Course.courseCode GROUP BY Programme.programmeID ORDER BY Programme.programmeID ")
-        totalCourse = cursor.fetchall()
-        cursor.execute("SELECT Course.CourseName FROM Course,ProgrammeCourse WHERE ProgrammeCourse.courseCode = Course.courseCode ORDER BY ProgrammeCourse.programmeID,Course.CourseName;")
-        course = cursor.fetchall()
-        return render_template('Programme.html',prog=progDetails,total=totalCourse,course=course)
+        cursor = db_conn.cursor(cursors.DictCursor)
+        query = "SELECT programmeName, courseName FROM Programme p, ProgrammeCourse d, Course c WHERE p.programmeID=d.programmeID AND d.courseCode=c.courseCode ORDER BY p.programmeID ASC, c.courseCode ASC"
+        cursor.execute(query)
+        courses = cursor.fetchall()
+
+        data = {}
+        for row in courses:
+            key = row["programmeName"]
+            if key not in data:
+                data[key] = []
+            value = row["courseName"]
+
+            # Convert to a list if it is not already.
+            if not isinstance(value, list):
+                value = [value]
+
+            data[key].extend(value)
+            
+        cursor.close()
+        return render_template('Programme.html', courses=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
