9764d1c4cd4c586f8812433d259735ac83ef9f4b
WalterDrinkWater <qingshui9362@gmail.com>
2023-09-19 20:59:05 +0800

    tab

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import os
 import boto3
 from config import *
 from io import BytesIO
-import numpy as np
+import cv2
+import pytesseract
+from collections import defaultdict
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "sem-sk"
@@ -26,14 +28,55 @@ def home():
 @app.route("/programme", methods=['GET', 'POST'])
 def Get_Programme():
     if request.method == 'GET':
+        # cursor = db_conn.cursor(cursors.DictCursor)
+        # query = "SELECT programmeName, courseName FROM Programme p, ProgrammeCourse d, Course c WHERE p.programmeID=d.programmeID AND d.courseCode=c.courseCode ORDER BY p.programmeID ASC, c.courseCode ASC"
+        # cursor.execute(query)
+        # courses = cursor.fetchall()
+
+        # data = {}
+        # for row in courses:
+        #     key = row["programmeName"]
+        #     if key not in data:
+        #         data[key] = []
+        #     value = row["courseName"]
+
+        #     # Convert to a list if it is not already.
+        #     if not isinstance(value, list):
+        #         value = [value]
+
+        #     data[key].extend(value)
+
         cursor = db_conn.cursor()
-        cursor.execute("SELECT * FROM Programme ORDER BY ProgrammeID")
-        progDetails = cursor.fetchall()
-        cursor.execute("SELECT COUNT(Programme.programmeID) FROM Programme,ProgrammeCourse,Course WHERE Programme.programmeID = ProgrammeCourse.programmeID AND ProgrammeCourse.courseCode = Course.courseCode GROUP BY Programme.programmeID ORDER BY Programme.programmeID ")
-        totalCourse = cursor.fetchall()
-        cursor.execute("SELECT Course.CourseName FROM Course,ProgrammeCourse WHERE ProgrammeCourse.courseCode = Course.courseCode ORDER BY ProgrammeCourse.programmeID,Course.CourseName;")
+        query = "SELECT * FROM Programme"
+        cursor.execute(query)
+        prog = cursor.fetchall()
+        print(prog[0])
+        cursor.close()
+        return render_template('Programme.html', prog=prog)
+@app.route("/progDetails/<progID>", methods=['GET', 'POST'])
+def Get_Programme_Details(progID):
+    if request.method == 'GET':        
+        cursor = db_conn.cursor()
+        progSql = "SELECT * FROM Programme WHERE ProgrammeID=%s"
+        cursor.execute(progSql,(progID))
+        prog = cursor.fetchall()
+        courseSql = "SELECT Course.* FROM Course,ProgrammeCourse,Programme WHERE Programme.programmeID = ProgrammeCourse.programmeID AND ProgrammeCourse.courseCode = Course.courseCode AND Programme.programmeID=%s ORDER BY CourseName"
+        cursor.execute(courseSql,(progID))
         course = cursor.fetchall()
-        return render_template('Programme.html',prog=progDetails,total=totalCourse,course=course)
+        reqSql = "SELECT * FROM QualificationSubject WHERE programmeID=%s ORDER BY qualificationName"
+        cursor.execute(reqSql,(progID))
+        tempReq = cursor.fetchall()
+        grouped_data = defaultdict(list)
+
+        for row in tempReq:
+            group_key = row[3]
+            grouped_data[group_key].append(row)
+        
+        return render_template('ProgDetails.html', prog=prog ,course=course, req=grouped_data)
+    
+# 
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
