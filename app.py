f6a0b84fd35f53f98e0844e6552411c06fc29c0a
Amos-tha <127003574+Amos-tha@users.noreply.github.com>
2023-09-22 22:41:20 +0800

    asdasdsad

--- app.py
+++ app.py
@@ -170,8 +170,6 @@ def View_Aca_Details():
 
     return render_template('AcademicianDetails.html', acaDetails=acaDetails)
 
-
-
 # @app.route("/test")
 # def scan_img():
 #     # Mention the installed location of Tesseract-OCR in your system
