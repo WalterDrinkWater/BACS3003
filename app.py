5bd9782bcc504082d4dc04c77d12d7d0d9796958
WalterDrinkWater <qingshui9362@gmail.com>
2023-09-23 05:21:28 +0800

    backup

--- app.py
+++ app.py
@@ -28,26 +28,8 @@ def home():
 @app.route("/programme", methods=['GET', 'POST'])
 def Get_Programme():
     if request.method == 'GET':
-        # cursor = db_conn.cursor(cursors.DictCursor)
-        # query = "SELECT programmeName, courseName FROM Programme p, ProgrammeCourse d, Course c WHERE p.programmeID=d.programmeID AND d.courseCode=c.courseCode ORDER BY p.programmeID ASC, c.courseCode ASC"
-        # cursor.execute(query)
-        # courses = cursor.fetchall()
-
-        # data = {}
-        # for row in courses:
-        #     key = row["programmeName"]
-        #     if key not in data:
-        #         data[key] = []
-        #     value = row["courseName"]
-
-        #     # Convert to a list if it is not already.
-        #     if not isinstance(value, list):
-        #         value = [value]
-
-        #     data[key].extend(value)
-
         cursor = db_conn.cursor()
-        query = "SELECT * FROM Programme"
+        query = "SELECT * FROM Programme ORDER BY programmeDuration"
         cursor.execute(query)
         prog = cursor.fetchall()
         print(prog[0])
@@ -75,17 +57,17 @@ def Get_Programme_Details(progID):
             group_key = row[3]
             grouped_data[group_key].append(row)
         cursor.close()
-        return render_template('ProgDetails.html', prog=prog ,course=course, req=grouped_data,ov=ov)
+        return render_template('ProgDetails.html',progID=progID, prog=prog ,course=course, req=grouped_data,ov=ov)
 
 @app.route("/progCompare/<progID>", methods=['GET', 'POST'])
 def Compare_Programme(progID):
     cursor = db_conn.cursor()    
     if request.method == 'GET':
-        mainProg = "Select programmeName from Programme WHERE programmeID = %s"
+        mainProg = "Select * from Programme WHERE programmeID = %s"
         cursor.execute(mainProg,(progID))
         mProg = cursor.fetchall()
-        progListSql = "Select programmeName,programmeID from Programme WHERE programmeID != %s"
-        cursor.execute(progListSql,(progID))
+        progListSql = "Select programmeName,programmeID from Programme WHERE programmeID != %s AND programmeType = %s"
+        cursor.execute(progListSql,(progID,mProg[0][4]))
         progList = cursor.fetchall()
         #overview1
         progOvSql = "SELECT programmeName, intakeYear,intakeMonth,programmeDuration,campusName FROM Programme,ProgrammeCampus,Intake,Campus WHERE Programme.programmeID = ProgrammeCampus.programmeID  AND Intake.IntakeID = ProgrammeCampus.intakeID AND Campus.campusID = ProgrammeCampus.campusID AND Programme.programmeID = %s"
@@ -127,83 +109,165 @@ def Compare_Programme(progID):
         prog_data = []
 
         # Iterate over the list of tuples and add the data to the list of dictionaries
-        for prog_name, alevel_sub, alevel_grade, diploma_grade in allReq:
+        for prog_name, level, subject, grade in allReq:
             prog_data.append({
             "Program": prog_name,
-            "Subject": alevel_sub,
-            "Grade": alevel_grade
+            "Level": level,
+            "Subject": subject,
+            "Grade": grade
             })
-        print(allReq)
+        allCoursesSql = "SELECT courseName FROM Course,Programme,ProgrammeCourse WHERE Programme.programmeID = ProgrammeCourse.programmeID AND ProgrammeCourse.courseCode = Course.courseCode AND Programme.programmeID = %s UNION SELECT courseName FROM Course,Programme,ProgrammeCourse WHERE Programme.programmeID = ProgrammeCourse.programmeID AND ProgrammeCourse.courseCode = Course.courseCode AND Programme.programmeID = %s"
+        cursor.execute(allCoursesSql,(progID,progList[0][1]))
+        allCourses = cursor.fetchall()
+
+        progNameSql = "Select programmeName from Programme WHERE programmeID=%s"
+        cursor.execute(progNameSql,(progList[0][1]))
+        cName = cursor.fetchall()
+        coursesSql = "SELECT courseName FROM Course,Programme,ProgrammeCourse WHERE Programme.programmeID = ProgrammeCourse.programmeID  AND ProgrammeCourse.courseCode = Course.courseCode AND Programme.programmeID = %s"
+        cursor.execute(coursesSql,progID)
+        courses1 = cursor.fetchall()
+        cursor.execute(coursesSql,(progList[0][1]))
+        courses2 = cursor.fetchall()
+
+    if request.method == 'POST':
+        cProgID = request.form["cProg"]
+        mainProg = "Select * from Programme WHERE programmeID = %s"
+        cursor.execute(mainProg,(progID))
+        mProg = cursor.fetchall()
+        progListSql = "Select programmeName,programmeID from Programme WHERE programmeID != %s AND programmeType = %s"
+        cursor.execute(progListSql,(progID,mProg[0][4]))
+        progList = cursor.fetchall()
+        #overview1
+        progOvSql = "SELECT programmeName, intakeYear,intakeMonth,programmeDuration,campusName FROM Programme,ProgrammeCampus,Intake,Campus WHERE Programme.programmeID = ProgrammeCampus.programmeID  AND Intake.IntakeID = ProgrammeCampus.intakeID AND Campus.campusID = ProgrammeCampus.campusID AND Programme.programmeID = %s"
+        cursor.execute(progOvSql,(progID))
+        mOv = cursor.fetchall()
+        progDict = {}
+        for prog in mOv:
+            if prog[0] not in progDict:
+                progDict[prog[0]] = {'intake': [], 'locations': []}
+            progDict[prog[0]]['intake'].append(f"{prog[1]}/{prog[2]}")
+
+            progDict[prog[0]]['locations'].append(prog[4])
+            progDict[prog[0]]['duration'] = prog[3]
+        progDict[mOv[0][0]]['intake'] = set(progDict[mOv[0][0]]['intake'])
+        progDict[mOv[0][0]]['locations'] = set(progDict[mOv[0][0]]['locations'])
+        
+        #overview2
+        progOvSql = "SELECT programmeName, intakeYear,intakeMonth,programmeDuration,campusName FROM Programme,ProgrammeCampus,Intake,Campus WHERE Programme.programmeID = ProgrammeCampus.programmeID  AND Intake.IntakeID = ProgrammeCampus.intakeID AND Campus.campusID = ProgrammeCampus.campusID AND Programme.programmeID = %s"
+        cursor.execute(progOvSql,(cProgID))
+        mOv = cursor.fetchall()
+        progDict1 = {}
+        for prog in mOv:
+            if prog[0] not in progDict1:
+                progDict1[prog[0]] = {'intake': [], 'locations': []}
+            progDict1[prog[0]]['intake'].append(f"{prog[1]}/{prog[2]}")
+            progDict1[prog[0]]['locations'].append(prog[4])
+            progDict1[prog[0]]['duration'] = prog[3]
+        progDict1[mOv[0][0]]['intake'] = set(progDict1[mOv[0][0]]['intake'])
+        progDict1[mOv[0][0]]['locations'] = set(progDict1[mOv[0][0]]['locations'])
+        combinedDict = {}
+        combinedDict.update(progDict)
+        combinedDict.update(progDict1)
+        sortedDict = sorted(combinedDict.items(), key=lambda x: x[0])
+
+        #compare requirement
+        reqSql = "(SELECT DISTINCT programmeName,qualificationName,subjectName,grade FROM QualificationSubject, Programme WHERE QualificationSubject.programmeID = Programme.programmeID AND Programme.programmeID=%s)UNION(SELECT DISTINCT programmeName,qualificationName,subjectName,grade FROM QualificationSubject, Programme WHERE QualificationSubject.programmeID = Programme.programmeID AND Programme.programmeID=%s)ORDER BY qualificationName"
+        cursor.execute(reqSql,(progID,cProgID))
+        allReq = cursor.fetchall()
+        prog_data = []
+
+        # Iterate over the list of tuples and add the data to the list of dictionaries
+        for prog_name, level, subject, grade in allReq:
+            prog_data.append({
+            "Program": prog_name,
+            "Level": level,
+            "Subject": subject,
+            "Grade": grade
+            })
+        allCoursesSql = "SELECT courseName FROM Course,Programme,ProgrammeCourse WHERE Programme.programmeID = ProgrammeCourse.programmeID AND ProgrammeCourse.courseCode = Course.courseCode AND Programme.programmeID = %s UNION SELECT courseName FROM Course,Programme,ProgrammeCourse WHERE Programme.programmeID = ProgrammeCourse.programmeID AND ProgrammeCourse.courseCode = Course.courseCode AND Programme.programmeID = %s"
+        cursor.execute(allCoursesSql,(progID,cProgID))
+        allCourses = cursor.fetchall()
+
+        progNameSql = "Select programmeName from Programme WHERE programmeID=%s"
+        cursor.execute(progNameSql,(cProgID))
+        cName = cursor.fetchall()
+        coursesSql = "SELECT courseName FROM Course,Programme,ProgrammeCourse WHERE Programme.programmeID = ProgrammeCourse.programmeID  AND ProgrammeCourse.courseCode = Course.courseCode AND Programme.programmeID = %s"
+        cursor.execute(coursesSql,progID)
+        courses1 = cursor.fetchall()
+        cursor.execute(coursesSql,(cProgID))
+        courses2 = cursor.fetchall()
     cursor.close()
-    return render_template("ProgCompare.html",mName = mProg,progList=progList, sDict = sortedDict,prog_data=prog_data)
+    return render_template("ProgCompare.html",progID=progID,mName = mProg,cName=cName,progList=progList, sDict = sortedDict,prog_data=prog_data,allCourses = allCourses,courses1 = courses1, courses2 = courses2)
 
-# @app.route("/test")
-# def scan_img():
-#     # Mention the installed location of Tesseract-OCR in your system
-#     pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
-    
-#     # Read image from which text needs to be extracted
-#     img = cv2.imread("static/media/test2.jpg")
-    
-#     # Preprocessing the image starts
-    
-#     # Convert the image to gray scale
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-#     # Performing OTSU threshold
-#     ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-    
-#     # Specify structure shape and kernel size.
-#     # Kernel size increases or decreases the area
-#     # of the rectangle to be detected.
-#     # A smaller value like (10, 10) will detect
-#     # each word instead of a sentence.
-#     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
-    
-#     # Applying dilation on the threshold image
-#     dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
-    
-#     # Finding contours
-#     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-#                                                     cv2.CHAIN_APPROX_NONE)
-    
-#     # Creating a copy of image
-#     im2 = img.copy()
-    
-#     # A text file is created and flushed
-#     file = open("recognized.txt", "w+")
-#     file.write("")
-#     file.close()
-    
-#     # Looping through the identified contours
-#     # Then rectangular part is cropped and passed on
-#     # to pytesseract for extracting text from it
-#     # Extracted text is then written into the text file
-#     for cnt in contours:
-#         x, y, w, h = cv2.boundingRect(cnt)
-        
-#         # Drawing a rectangle on copied image
-#         rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        
-#         # Cropping the text block for giving input to OCR
-#         cropped = im2[y:y + h, x:x + w]
-        
-#         # Open the file in append mode
-#         file = open("recognized.txt", "a")
-        
-#         # Apply OCR on the cropped image
-#         text = pytesseract.image_to_string(cropped)
-        
-#         # Appending the text into file
-#         file.write(text)
-#         file.write("\n")
-        
-#         # Close the file
-#         file.close
-#         cv2.imshow("Lena Soderberg”, img)
-#         cv2.waitKey(0)
-#     return "abc"
+@app.route("/admin/viewip", methods=['GET'])
+def Admin_View_IP():
+    return render_template('ViewIP.html')
 
+@app.route("/admin/getip", methods=["POST"])
+def Admin_Get_IP():
+    try:
+        db_conn2 = connections.Connection(
+            host=customhost,
+            port=3306,
+            user=customuser,
+            password=custompass,
+            db=customdb,
+            cursorclass=cursors.DictCursor,
+        )
+        cursor = db_conn2.cursor()
+        if request.method == "POST":
+            draw = request.form["draw"]
+            row = int(request.form["start"])
+            rowperpage = int(request.form["length"])
+            searchValue = request.form["search[value]"]
+
+            ## Total number of records without filtering
+            cursor.execute("SELECT count(*) as allcount from LoginSession")
+            rsallcount = cursor.fetchone()
+            totalRecords = rsallcount["allcount"]
+            searchValue = "%" + searchValue + "%"
+            ## Total number of records with filtering
+            cursor.execute("SELECT count(*) as allcount from LoginSession WHERE ipAddress LIKE %s",(searchValue))
+            rsallcount = cursor.fetchone()
+            totalRecordwithFilter = rsallcount["allcount"]
+
+            ## Fetch records
+            if searchValue == "":
+                cursor.execute("SELECT * FROM LoginSession ORDER BY loginTime limit %s, %s;",(row, rowperpage))
+            else:
+                cursor.execute(
+                    "SELECT * FROM LoginSession WHERE ipAddress LIKE %s ORDER BY loginTime limit %s, %s;",(searchValue,row,rowperpage))
+            offerlist = cursor.fetchall()
+            data = []
+            for row in offerlist:
+                data.append(
+                    {
+                        "ip": row["ipAddress"],
+                        "id": row["accountID"],                          
+                        "loginTime": row["loginTime"],
+                        "logoutTime": row["logoutTime"],                      
+                    }
+                )
+            response = {
+                "draw": draw,
+                "iTotalRecords": totalRecords,
+                "iTotalDisplayRecords": totalRecordwithFilter,
+                "aaData": data,
+            }
+            return jsonify(response)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        db_conn2.close()
+
+@app.route("/admin/userdetails/<id>", methods=["GET"])
+def Admin_Get_User_Details(id):
+    cursor = db_conn.cursor()    
+    userSql = 'SELECT * FROM Account WHERE accountID=%s'
+    cursor.execute(userSql,(id))
+    user= cursor.fetchone()
+    return render_template('UserDetails.html', user=user)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
