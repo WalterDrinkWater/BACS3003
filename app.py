ccf83c2cce8f4ee70940d84221e4da8ec1d88e5d
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-09-28 02:24:32 +0800

    bug solved, doin another thing

--- app.py
+++ app.py
@@ -414,14 +414,16 @@ def uploadic():
             path = os.path.join("static/media/" + id + "_front_" + icf.filename)
             icf.save(path)
             cursor.execute("UPDATE Applications SET identificationFrontPath = %s WHERE applicationID=%s",(path, id))
+            session["icf"] = "pass"
 
         if(icf != None):
             path = os.path.join("static/media/" + id + "_back" + icb.filename)
             icf.save(path)
             cursor.execute("UPDATE Applications SET identificationBackPath = %s WHERE applicationID=%s",(path, id))
+            session["icb"] = "pass"
 
         db_conn.commit()
-
+        
     except Exception as e:
         return str(e)
 
@@ -433,9 +435,12 @@ def uploadic():
 @app.route("/application/intake", methods=['GET', 'POST'])
 def intake():
     if request.method == 'GET':
-        if request.args.get("id") != None and  request.args.get("status") == 'edit':
-            appid = request.args.get("id")
-            session["appid"] = appid
+        if request.args.get("status") == 'edit':
+            if request.args.get("id") != None:
+                appid = request.args.get("id")
+                session["appid"] = appid
+            else:
+                appid = session["appid"]
         
             try:
                 cursor = db_conn.cursor(cursors.DictCursor)
@@ -467,6 +472,9 @@ def intake():
                     for _ in range (3):
                         cursor.execute("INSERT INTO ApplicationProgramme (applicationID) VALUES (%s)", (application["applicationID"]))
                         db_conn.commit()
+                    session["intake"] = "fail"
+                    session["icf"] = "fail"
+                    session["icb"] = "fail"
                 except Exception as e:
                     return str(e)
 
@@ -507,6 +515,7 @@ def apply_intake():
             cursor.execute("UPDATE ApplicationProgramme SET applicationID = %s, programmeCampusID = %s WHERE apID = %s",
                         (appid, pci[0], api))
             db_conn.commit()
+        session["intake"] = "pass"
 
     except Exception as e:
         return str(e)
@@ -565,11 +574,16 @@ def dynamic_selection():
 
 @app.route('/application/qualification')
 def qualification():
+    intakePart = session["intake"]
+    icfPart = session["icf"]
+    icbPart = session["icb"]
     return render_template("Qualification.html")
 
 @app.route('/application/assess', methods=['POST'])
 def assess_qualification():
     id = session["appid"]
+    intakePart = session["intake"]
+    icPart = session["ic"]
     spmObj = request.files["qualification"].read()
     diploma = request.files["diploma"].read()
     if spmObj:
